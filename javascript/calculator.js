cc5e502b959cdfea6871c51872690b256c7c864d
Gina <paalginaa@gmail.com>
2017-06-15 12:29:29 +0200

    add and subtract implemented

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -5,3 +5,27 @@ function insertDigit(digit) {
 function reset() {
     document.getElementById("input").value = "";
 }
+
+function doTheMath() {
+    var inputDatas = document.getElementById("input").value;
+    var inputLen = inputDatas.length;
+
+    var addPos = inputDatas.indexOf("+");
+    var subPos = inputDatas.indexOf("-");
+    var multiplyPos = inputDatas.indexOf("*");
+    var divPos = inputDatas.indexOf("/");
+
+    if (addPos >= 0) {
+        var x = inputDatas.substring(0, addPos);
+        var y = inputDatas.slice(addPos + 1, inputLen);
+        var result = parseFloat(x) + parseFloat(y);
+        document.getElementById("input").value = result
+    }
+
+    else if (subPos >= 0) {
+        var x = inputDatas.substring(0, subPos);
+        var y = inputDatas.slice(subPos + 1, inputLen)
+        var result = parseFloat(x) - parseFloat(y);
+        document.getElementById("input").value = result;
+    }
+}
